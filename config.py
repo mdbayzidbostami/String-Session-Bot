67490dbaeffa3eaa6779d64f97326f27d343d43a
mdbayzidbostami <bayzid.simran@gmail.com>
2026-01-29 02:03:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+API_ID = os.environ.get("API_ID", "22005506")
+API_HASH = os.environ.get("API_HASH", "af9406c860b34bd575b04f2b555bb6b0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8377038535:AAF8P00IYaqXibyrkKLSe6jcWPgizVlAQbc")
+ADMIN = int(os.environ.get("ADMIN", "5548923721"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002549272838"))
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://xeroxbayzid12:xeroxbayzid12@cluster0.dcapc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "xeroxbayzid12")
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******* -100******").split())) # Add Multiple channel ids
-AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNEL", "-100******* -100*******").split())) # Add Multiple channel ids
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002549272838").split())) # Add Multiple channel ids
+AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNEL", "-1002549272838").split())) # Add Multiple channel ids
 FSUB_EXPIRE = int(os.environ.get("FSUB_EXPIRE", 2))  # minutes, 0 = no expiry
